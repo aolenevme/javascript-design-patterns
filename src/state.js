c2c5013529bb7562b5db294153cb40a56036c2c8
Alexander Olenev <olenev.biz@gmail.com>
2020-06-25 22:12:05 +0300

    feat/add-state
    Added state pattern

---
+++ src/state.js
@@ -0,0 +1,9 @@
+/**
+ * Allow an object to alter its behavior when its internal state changes.
+ * The object will appear to change its class.
+ *
+ * In JS world: State is very close to the Strategy. They even have the same UML
+ * diagrams. It achieves the same goal using another mechanism. Instead of
+ * providing strategy explicitly, it depends on some state. From JS perspective
+ * it can be implemented the same way as strategy pattern.
+ */
