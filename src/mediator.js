8a43d6cd8af7e0aafc45890d47f186589b973874
eshekak <olenev.biz@gmail.com>
2020-07-01 21:09:40 +0300

    feat/add-mediator: Added

---
+++ src/mediator.js
@@ -0,0 +1,35 @@
+/**
+ * Define an object that encapsulates how a set of objects interact. Mediator
+ * promotes loose coupling by keeping objects from referring to each other
+ * explicitly, and it lets you vary their interaction independently.
+ *
+ * In JS world: It is just a one approach to reduce coupling
+ */
+
+const mediator = {
+  users: [],
+
+  send(users, text) {
+    users.forEach((user) => {
+      receive(user, text);
+    });
+  },
+};
+
+function receive(user, text) {
+  console.log(`${user.name} received the message: ${text}`);
+}
+
+function addUser(user) {
+  mediator.users = [...mediator.users, user];
+}
+
+function sendMessage(user, text) {
+  const { users, send } = mediator;
+
+  send(users, `I am ${user.name}. ${text}`);
+}
+
+addUser({ name: "Kent Beck" });
+addUser({ name: "Bob Martin" });
+sendMessage({ name: "Sasha" }, "Hello, there!");
