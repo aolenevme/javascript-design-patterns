a11361977e2695181fa54b5a1bf312e91663f7f7
Alexander Olenev <olenev.biz@gmail.com>
2020-07-06 12:48:02 +0300

    feat/add-bridge

---
+++ src/bridge.js
@@ -0,0 +1,75 @@
+/**
+ * Decouple an abstraction from its implementation so that the two can vary
+ * independently.
+ *
+ * In JS world: There is no bridge here, but abstraction and implementation can
+ * live independently. So we can use composite and visitor to mimic it. Plus,
+ * we can use mock parental implementations if new one doesn`t exist yet.
+ */
+
+const { defMulti, defMethod } = require("./common/multiple-dispatch.js");
+
+const jobHierarchy = [
+  "programmer-job",
+  [
+    ["java-job", ["senior-java-job"]],
+    ["clojure-job", ["senior-clojure-job"]],
+  ],
+];
+
+function findParentJobName(jobName, subtree) {
+  if (isJobNameExistInNode(jobName, subtree)) {
+    return subtree[0];
+  }
+
+  for (const node of subtree) {
+    if (isJobNameExistInNode(jobName, node)) {
+      return node[0];
+    }
+  }
+
+  for (const node of subtree) {
+    if (Array.isArray(node)) {
+      return findParentJobName(jobName, node);
+    }
+  }
+}
+
+function isJobNameExistInNode(jobName, node) {
+  return Array.isArray(node) && node.flat(1).includes(jobName);
+}
+
+function regularJob(...methodArguments) {
+  const [jobName] = methodArguments;
+
+  return [jobName === jobHierarchy[0], methodArguments];
+}
+
+function clojureJob(...methodArguments) {
+  const [jobName] = methodArguments;
+
+  return [jobName === jobHierarchy[1][1][0], methodArguments];
+}
+
+function adHocJob(...methodArguments) {
+  const [jobName] = methodArguments;
+
+  const parentJobName = findParentJobName(jobName, jobHierarchy);
+
+  return [Boolean(parentJobName), [parentJobName]];
+}
+
+function filterRequirementsByJobName(jobName) {
+  return defMulti(
+    defMethod(regularJob(jobName), () => "Requirements for the programmer job"),
+    defMethod(clojureJob(jobName), () => "Requirements for the clojure job"),
+    defMethod(adHocJob(jobName), (parentJobName) =>
+      filterRequirementsByJobName(parentJobName)
+    ),
+    defMethod([true, []], () => `Sorry, there is requirements ${jobName} :(`)
+  )();
+}
+
+console.log(filterRequirementsByJobName("programmer-job"));
+console.log(filterRequirementsByJobName("senior-clojure-job"));
+console.log(filterRequirementsByJobName("logistic-job"));
---
+++ src/proxy.js
@@ -0,0 +1,7 @@
+/**
+ * Surrogate. Provide a surrogate or placeholder for another object to control
+ * access to it.
+ *
+ * In JS world: It is just wrapper, function composition. Have a look at
+ * Proxy API for examples of usage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+ */
