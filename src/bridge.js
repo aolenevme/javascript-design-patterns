84444b2b1b92338be29e57b26e16b6d044a93d5d
eshekak <olenev.biz@gmail.com>
2020-07-06 10:27:49 +0300

    feat/add-bridge: Added template implementation

--- src/bridge.js
+++ src/bridge.js
@@ -5,3 +5,56 @@
  * In JS world: There is no bridge here, but abstraction and implementation can
  * live independently.
  */
+
+const { defMulti, defMethod } = require("./common/multiple-dispatch.js");
+
+const hierarchy = [
+  "job",
+  [
+    ["clojure-job", ["senior-clojure-job"]],
+    ["java-job", ["senior-java-job"]],
+  ],
+];
+
+function regularJob(...methodArguments) {
+  const [jobName] = methodArguments;
+
+  return [jobName === hierarchy[0], methodArguments];
+}
+
+function adHocJob(...methodArguments) {
+  const [jobName] = methodArguments;
+
+  const [isJobExistInHierarchy, parentJobName] = findParentJobName(jobName);
+
+  return [isJobExistInHierarchy, [parentJobName]];
+}
+
+function findParentJobName(jobName) {
+  const flattedJobHierarchy = hierarchy.flat(3);
+
+  const isJobExistInHierarchy = flattedJobHierarchy.includes(jobName);
+
+  if (!isJobExistInHierarchy) {
+    return [false, ""];
+  }
+
+  return [true, "job"];
+}
+
+function filterPositionByJobName(jobName) {
+  return defMulti(
+    defMethod(
+      regularJob(jobName),
+      () => `Check requirements for the ${jobName}`
+    ),
+    defMethod(adHocJob(jobName), (parentJobName) =>
+      filterPositionByJobName(parentJobName)
+    ),
+    defMethod([true, []], () => `Sorry, there is no such job: ${jobName} :(`)
+  )();
+}
+
+console.log(filterPositionByJobName("job"));
+console.log(filterPositionByJobName("senior-clojure-job"));
+console.log(filterPositionByJobName("non-existing-job"));
