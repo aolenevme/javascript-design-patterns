ddc27d969858f136d40dfc1d49ca606c4b62a249
eshekak <olenev.biz@gmail.com>
2020-07-06 09:11:36 +0300

    feat/add-proxy: Added

---
+++ src/proxy.js
@@ -0,0 +1,6 @@
+/**
+ * Surrogate. Provide a surrogate or placeholder for another object to control
+ * access to it.
+ *
+ * In JS world: Look at Proxy API for examples of usage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+ */
