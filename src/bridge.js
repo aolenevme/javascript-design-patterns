12b05ef572c9453557a98adcb7c99e1fb4625844
eshekak <olenev.biz@gmail.com>
2020-07-06 09:32:43 +0300

    feat/add-bridge: Added description

---
+++ src/bridge.js
@@ -0,0 +1,7 @@
+/**
+ * Decouple an abstraction from its implementation so that the two can vary
+ * independently.
+ *
+ * In JS world: There is no bridge here, but abstraction and implementation can
+ * live independently.
+ */
--- src/proxy.js
+++ src/proxy.js
@@ -2,5 +2,6 @@
  * Surrogate. Provide a surrogate or placeholder for another object to control
  * access to it.
  *
- * In JS world: Look at Proxy API for examples of usage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+ * In JS world: It is just wrapper, function composition. Have a look at
+ * Proxy API for examples of usage: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
  */
